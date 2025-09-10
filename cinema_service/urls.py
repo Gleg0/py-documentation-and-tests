0ba3e6810cdf71a08ab807ea8198b4d3c50402b5
Glego <glego200323@gmail.com>
2025-09-10 22:26:19 +0300

    Solution small fix

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView, TokenRefreshView
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -21,15 +18,5 @@ urlpatterns = [
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
-    ),
-    path(
-        "api/token/",
-        TokenObtainPairView.as_view(),
-        name="token_obtain_pair"
-    ),
-    path(
-        "api/token/refresh/",
-        TokenRefreshView.as_view(),
-        name="token_refresh"
     ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
