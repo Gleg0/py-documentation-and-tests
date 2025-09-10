e855c02096d33b3f14ed451fae018246042093b3
Glego <glego200323@gmail.com>
2025-09-11 02:30:33 +0300

    Solution fix

--- cinema/tests/test_movie_api.py
+++ cinema/tests/test_movie_api.py
@@ -276,3 +276,29 @@ class MovieApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["title"], movie1.title)
 
+
+class JWTAuthenticationTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            email="user@test.com",
+            password="password"
+        )
+
+    def test_jwt_authentication_flow(self):
+        url = reverse("user:token_obtain_pair")
+        res = self.client.post(url, {"email": self.user.email, "password": "password"}, format="json")
+        self.assertEqual(res.status_code, 200)
+        access = res.data["access"]
+
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {access}")
+        res = self.client.get(MOVIE_URL)
+        self.assertEqual(res.status_code, 200)
+
+        self.client.credentials()
+        res = self.client.get(MOVIE_URL)
+        self.assertEqual(res.status_code, 401)
+
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer invalidtoken")
+        res = self.client.get(MOVIE_URL)
+        self.assertEqual(res.status_code, 401)
